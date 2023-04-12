62f554d71d3dff5e21ba6319e5eb34e289161a75
Sławomir Cielepak <slawomir.cielepak@gmail.com>
2023-04-12 13:08:31 +0200

    Add .gitconfig bind

--- run.sh
+++ run.sh
@@ -12,4 +12,5 @@ docker run -it --rm \
  --user=rosdev \
  --mount type=bind,source=$HOME/git,target=/home/rosdev/git \
  --mount type=bind,source=$HOME/.ssh,target=/home/rosdev/.ssh \
+ --mount type=bind,source=$HOME/.gitconfig,target=/home/rosdev/.gitconfig \
  ros-humble-desktop-nvidia "$@"
